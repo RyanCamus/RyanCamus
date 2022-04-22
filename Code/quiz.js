313e9f448b300effbfe6ce46351700bd66e13952
Ryan <camus.ryan@gmail.com>
2022-04-22 17:59:29 +0800

    Add all current files

---
+++ Code/quiz.js
@@ -0,0 +1,21 @@
+
+
+/*margarita 
+
+bitter = margarita*3 + negroni*5 + mojito*1
+
+Refer to a database, where each cocktail/ingredient is assigned bitterness, sweetness, etc values
+These values can then be automatically retrieved for each ingredient used in the quiz
+*/
+console.log('say something');
+
+let margaritaB = 3;
+let negroniB = 5;
+
+let margarita = false;
+let negroni = false;
+
+negroni = true;
+
+bitterness = (margaritaB * margarita) + (negroniB * negroni );
+console.log(`bitterness preference = ${bitterness}.`);
